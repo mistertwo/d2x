dxx-rebirth/dxx-rebirth

Made hmp code actually read tempo. Due to missing documentation of the format I can only guess it's a 4 bit int

misc/hmp.c
@@ -44,7 +44,7 @@ void hmp_close(hmp_file *hmp)
 }
 
 hmp_file *hmp_open(const char *filename) {
-	int i, data, num_tracks;
+	int i, data, num_tracks, tempo;
 	char buf[256];
 	PHYSFS_file *fp;
 	hmp_file *hmp;
@@ -88,9 +88,21 @@ hmp_file *hmp_open(const char *filename) {
 		hmp_close(hmp);
 		return NULL;
 	}
-
 	hmp->num_trks = num_tracks;
-	hmp->tempo = 120;
+
+	if (PHYSFSX_fseek(fp, 0x38, SEEK_SET))
+	{
+		PHYSFS_close(fp);
+		hmp_close(hmp);
+		return NULL;
+	}
+	if (PHYSFS_read(fp, &tempo, 4, 1) != 1)
+	{
+		PHYSFS_close(fp);
+		hmp_close(hmp);
+		return NULL;
+	}
+	hmp->tempo = tempo;
 
 	if (PHYSFSX_fseek(fp, 0x308, SEEK_SET))
 	{
@@ -720,6 +732,7 @@ void hmp2mid(char *hmp_name, unsigned char **midbuf, unsigned int *midlen)
 		return;
 
 	*midlen = 0;
+	time_div = hmp->tempo*1.6;
 
 	// write MIDI-header
 	*midbuf = d_realloc(*midbuf, *midlen + 4);

mistertwo/d2x

misc/hmp.c
@@ -81,9 +81,21 @@ hmp_file *hmp_open(const char *filename) {
 		hmp_close(hmp);
 		return NULL;
 	}
-
 	hmp->num_trks = num_tracks;
-	hmp->tempo = 120;
+
+	if (cfseek(fp, 0x38, SEEK_SET))
+	{
+		cfclose(fp);
+		hmp_close(hmp);
+		return NULL;
+	}
+	if (cfread(&tempo, 4, 1, fp) != 1)
+	{
+		cfclose(fp);
+		hmp_close(hmp);
+		return NULL;
+	}
+	hmp->tempo = tempo;
 
 	if (cfseek(fp, 0x308, SEEK_SET))
 	{
@@ -229,6 +241,7 @@ void hmp2mid(char *hmp_name, unsigned char **midbuf, unsigned int *midlen)
 		return;
 
 	*midlen = 0;
+	time_div = hmp->tempo*1.6;
 
 	// write MIDI-header
 	*midbuf = d_realloc(*midbuf, *midlen + 4);